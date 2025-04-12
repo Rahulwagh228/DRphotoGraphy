6711c0396a26e976a89e9e74412957ced888f1db
Rahulwagh228 <127666835+Rahulwagh228@users.noreply.github.com>
2025-04-13 02:10:10 +0530

    minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from '@/component/Navbar/Navbar'
 import HeroSection from '@/component/hero-section/HeroSection'
 import HeroSection2 from '@/component/hero-section2/heroSection2'
-import Footer from '@/component/footer/page'
 
 const page = () => {
   return (

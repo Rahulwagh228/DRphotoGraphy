806bd3659cc6c81aadec0bdce05a5990c39755c1
Rahulwagh228 <127666835+Rahulwagh228@users.noreply.github.com>
2025-04-12 17:49:19 +0530

    Navbar complited and setup of fonts and logo images

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navbar from '@/component/Navbar/Navbar'
 
 const page = () => {
   return (
     <>
-      <div>
-
+      <Navbar />
+      <div className="pt-16">
+        {/* Content will go here */}
       </div>
     </>
   )
---
+++ component/Navbar/Navbar.tsx
@@ -0,0 +1,66 @@
+'use client'
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { FiMenu, FiX } from 'react-icons/fi'
+import Logo from '@/public/assets/Logo-updated.png'
+import Image from 'next/image'
+import styles from './css/Navbar.module.scss'
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const menuItems = [
+    { label: 'Services', href: '/' },
+    { label: 'Portfolio', href: '/' },
+    { label: 'Testimonials', href: '/' },
+  ]
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbarContainer}>
+        <Link href="/" className={styles.logoContainer}>
+          <Image src={Logo} alt="Logo" width={80} height={80} className={styles.logoImage} />
+        </Link>
+
+        {/* Desktop Menu */}
+        <div className={styles.menuDesktop}>
+          {menuItems.map((item) => (
+            <Link
+              key={item.label}
+              href={item.href}
+              className={styles.menuItem}
+            >
+              {item.label}
+            </Link>
+          ))}
+        </div>
+
+        {/* Mobile Menu Button */}
+        <button
+          className={styles.menuButton}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? <FiX size={32} /> : <FiMenu size={32} />}
+        </button>
+      </div>
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <div className={styles.menuMobile}>
+          {menuItems.map((item) => (
+            <Link
+              key={item.label}
+              href={item.href}
+              className={styles.menuItem}
+              onClick={() => setIsOpen(false)}
+            >
+              {item.label}
+            </Link>
+          ))}
+        </div>
+      )}
+    </nav>
+  )
+}
+
+export default Navbar
